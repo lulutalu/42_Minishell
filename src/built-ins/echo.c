9148a7a2f0b10242a74e88a4ba981ec3ab0ea1d1
Loic Dubouloz <lulutalu@gmail.com>
2022-06-05 16:29:59 +0200

    Little modifications. Normed current srcs with modification to the comments. Deleted my old version of simple_quote.c inside the parsing as this work is now my teammate's

--- src/built-ins/echo.c
+++ src/built-ins/echo.c
@@ -6,24 +6,30 @@
 /*   By: lduboulo <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 23:20:31 by lduboulo          #+#    #+#             */
-/*   Updated: 2022/06/05 15:44:36 by lduboulo         ###   ########.fr       */
+/*   Updated: 2022/06/05 16:29:01 by lduboulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../includes/minishell.h"
 
 /* Pseudo-code as changes are expected once the parsing is done */
+/*With the parsing done, there might be some adjustment in here*/
+/*Might need to check for commands like "echo ~"*/
+/*As of right now, the idea is to receive some simple boolean*/
+/*that tell wich operation should be done based on the parsing*/
 void	b_echo(t_main *main)
 {
-	int	no_arg = 0;
-	int	n_arg = 0;
+	int	no_arg;
+	int	n_arg;
 
+	no_arg = 0;
+	n_arg = 0;
 	if (no_arg == 1)
 		printf("\n");
 	else if (n_arg == 1)
-		printf("%s", main->input);		//use of main->input will change (parsing)
+		printf("%s", main->input);
 	else
 		printf("%s\n", main->input);
-	g_exit_status = 0;					//never forget to set correct value for exit code
+	g_exit_status = 0;
 }
 //what about echo ~ ?
--- src/env/env.c
+++ src/env/env.c
@@ -6,70 +6,22 @@
 /*   By: lduboulo <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 12:37:05 by lduboulo          #+#    #+#             */
-/*   Updated: 2022/06/04 17:21:13 by lduboulo         ###   ########.fr       */
+/*   Updated: 2022/06/05 16:26:17 by lduboulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/*Function to duplicate the **envp variable at the beginning of the programm*/
+/*That way, the environnment can be modified during the programm*/
+/*This function might be moved in the future*/
 void	env_dup(t_main *main, char **envp)
 {
 	int		i;
-//	t_node	*cur;						//uncomment for test
 
 	main->head_env = NULL;
 	main->tail_env = NULL;
 	i = 0;
-	while (envp[i]) 					//while envp not NULL to process every line
+	while (envp[i])
 		lst_add(&main->head_env, &main->tail_env, envp[i++]);
-/*	cur = main->head_env;
-	while (cur != NULL)
-	{
-		printf("%s=%s\n", cur->var, cur->value);
-		cur = cur->next;
-	}*/ 								//uncomment for test
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
